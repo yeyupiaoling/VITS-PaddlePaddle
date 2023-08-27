b6421f3fd27e278d58c490aeb8068b1f3d5927f9
yeyupiaoling <yeyupiaoling@foxmail.com>
2023-08-27 13:21:01 +0800

    修改一些代码

--- ppvits/data_utils/mel_processing.py
+++ ppvits/data_utils/mel_processing.py
@@ -1,4 +1,5 @@
 import paddle
+from paddle.nn import functional as F
 from librosa.filters import mel as librosa_mel_fn
 
 MAX_WAV_VALUE = 32768.0
@@ -45,11 +46,11 @@ def spectrogram_paddle(y, n_fft, sampling_rate, hop_size, win_size, center=False
     global hann_window
     wnsize_dtype = str(win_size)
     if wnsize_dtype not in hann_window:
-        hann_window[wnsize_dtype] = paddle.audio.functional.get_window(window='hann', win_length=win_size, dtype=y.dtype)
-    # TODO
-    # y = paddle.nn.functional.pad(y.unsqueeze(1), (int((n_fft - hop_size) / 2), int((n_fft - hop_size) / 2)),
-    #                              mode='reflect')
-    # y = y.squeeze(1)
+        hann_window[wnsize_dtype] = paddle.audio.functional.get_window(window='hann', win_length=win_size,
+                                                                       dtype=y.dtype)
+    y = F.pad(y.unsqueeze(1), (int((n_fft - hop_size) / 2), int((n_fft - hop_size) / 2)),
+              mode='reflect', data_format="NCL")
+    y = y.squeeze(1)
 
     spec = paddle.signal.stft(y, n_fft, hop_length=hop_size, win_length=win_size,
                               window=hann_window[wnsize_dtype],
@@ -78,26 +79,24 @@ def mel_spectrogram_paddle(y, n_fft, num_mels, sampling_rate, hop_size, win_size
         print('max value is ', paddle.max(y))
 
     global mel_basis, hann_window
-    dtype_device = str(y.dtype) + '_' + str(y.device)
-    fmax_dtype_device = str(fmax) + '_' + dtype_device
-    wnsize_dtype_device = str(win_size) + '_' + dtype_device
-    if fmax_dtype_device not in mel_basis:
+    fmax_dtype = str(fmax) + '_' + str(y.dtype)
+    wnsize_dtype = str(win_size) + '_' + str(y.dtype)
+    if fmax_dtype not in mel_basis:
         mel = librosa_mel_fn(sr=sampling_rate, n_fft=n_fft, n_mels=num_mels, fmin=fmin, fmax=fmax)
-        mel_basis[fmax_dtype_device] = paddle.to_tensor(mel, dtype=y.dtype)
-    if wnsize_dtype_device not in hann_window:
-        hann_window[wnsize_dtype_device] = paddle.to_tensor(win_size, dtype=y.dtype)
+        mel_basis[fmax_dtype] = paddle.to_tensor(mel, dtype=y.dtype)
+    if wnsize_dtype not in hann_window:
+        hann_window[wnsize_dtype] = paddle.to_tensor(win_size, dtype=y.dtype)
 
-    y = paddle.nn.functional.pad(y.unsqueeze(1), (int((n_fft - hop_size) / 2), int((n_fft - hop_size) / 2)),
-                                 mode='reflect')
+    y = F.pad(y.unsqueeze(1), (int((n_fft - hop_size) / 2), int((n_fft - hop_size) / 2)), mode='reflect')
     y = y.squeeze(1)
 
     spec = paddle.signal.stft(y.float(), n_fft, hop_length=hop_size, win_length=win_size,
-                              window=hann_window[wnsize_dtype_device],
-                              center=center, pad_mode='reflect', normalized=False, onesided=True)
+                              window=hann_window[wnsize_dtype], center=center, pad_mode='reflect',
+                              normalized=False, onesided=True)
     spec = paddle.as_real(spec)
     spec = paddle.norm(spec, p=2, axis=-1)
 
-    spec = paddle.matmul(mel_basis[fmax_dtype_device], spec)
+    spec = paddle.matmul(mel_basis[fmax_dtype], spec)
     spec = spectral_normalize_paddle(spec)
 
     return spec
